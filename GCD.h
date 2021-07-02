8c93a2ade7df318668f90011c7d17c5d80bb78d3
Jackie890621 <jackie890621@gmail.com>
2021-07-02 20:22:34 +0800

    --

---
+++ GCD.h
@@ -0,0 +1,19 @@
+// 
+// Gcd.h
+// DS hw2 array gcd
+//
+
+#ifndef _GCD_
+#define _GCD_
+#define ARRAY_MAX 1000
+using namespace std;
+class GCD {
+	private:
+
+	public:
+	GCD(){}
+	~GCD(){}
+	long* FindGCD(long *in1,long *in2,int deg1,int deg2);
+};
+#endif
+
